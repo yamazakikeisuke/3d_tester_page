f0a184ef369595f70c8fb21764d00681902569ed
keisuke_z <keisukedj9@gmail.com>
2024-08-08 18:30:29 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    const width = 960;
+    const height = 540;
+
+    // レンダラーを作成
+    const canvasElement = document.querySelector('#myCanvas');
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas: canvasElement,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    // シーンを作成
+    const scene = new THREE.Scene();
+
+    // カメラを作成
+    const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    camera.position.set(0, 0, 600);
+
+    // カメラコントローラーを作成
+    const controls = new THREE.OrbitControls(camera, canvasElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.2;
+
+    // 環境光源を作成
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    ambientLight.intensity = 0.5;
+    scene.add(ambientLight);
+
+    // 平行光源を作成
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.intensity = 1;
+    directionalLight.position.set(1, 3, 1);
+    scene.add(directionalLight);
+
+    // 3Dモデルの読み込み
+    const objLoader = new THREE.OBJLoader();
+    objLoader.load(
+        './test_____.obj',
+        function (obj) {
+            scene.add(obj);
+            obj.position.x = -50;
+            obj.position.y = -100;
+        },
+    );
+
+    tick();
+
+    function tick() {
+        renderer.render(scene, camera); // レンダリング
+        requestAnimationFrame(tick);
+    }
+}
